d56df6b7fedd31700ffe2a17f715d3c5c8c845a4
WLudy1012 <ludy111012@gmail.com>
2026-08-20 15:52:05 +0800

    chore: bump version to 1.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.mslx.console"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.2.5"
+        versionCode = 9
+        versionName = "1.2.6"
     }
 
     signingConfigs {
